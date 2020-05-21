40c69085b40288b9714d5b907e05bd9038851e24
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-05-21 17:13:48 +0100

    Updated the README file

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# MyRead: Book Reading App
 
-# Project Title
+MyRead: Book Reading App is an application that helps users to keep track of the books they reading by grouping the books in different order like currently reading, want to read, and read. The application was built with React.js.
 
-MyRead: A Book Reading App
+## Features
 
-## Available Scripts
+* Ability to search a book.
+* Ability to remove a book.  
+* Ability to add book from search.
 
-In the project directory, you can run:
 
-### `yarn start`
+## Instructions on how to install
 
-Runs the app in the development mode.<br />
+Please follow the instructions below to run or install the application:
+
+* Clone the project using `git clone`.
+* On the root folder of the project run `npm install` or `yarn install` to add dependencies of the project.
+* Then run `npm run start` or `yarn start` to start the project. 
+
+### Note:
+
+The application will run on development mode.
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -12,24 +12,24 @@ class BookShelf extends React.Component {
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{title}</h2>
-                    <div className="bookshelf-books">
-                        <BookGrid>
-                            {filteredBooks.map(book => (
-                                <BookGrid.Item key={book.id}>
-                                    <div className="book">
-                                        <div className="book-top">
-                                            <BookCover url={book.imageLinks.thumbnail} />
-                                            <SelectDropDown book={book} moveBook={(e) => onChangeBook(e, book)} />
-                                        </div>
-                                        <div className="book-title">{book.title}</div>
-                                        <div className="book-authors">
-                                            {book.authors.map((author,index) => <span key={index}>{author}</span>)}
-                                        </div>
+                <div className="bookshelf-books">
+                    <BookGrid>
+                        {filteredBooks.map(book => (
+                            <BookGrid.Item key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <BookCover url={book.imageLinks.thumbnail} />
+                                        <SelectDropDown book={book} moveBook={(e) => onChangeBook(e, book)} />
                                     </div>
-                                </BookGrid.Item>
-                            ))}
-                        </BookGrid>
-                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">
+                                        {book.authors.map((author,index) => <span key={index}>{author}</span>)}
+                                    </div>
+                                </div>
+                            </BookGrid.Item>
+                        ))}
+                    </BookGrid>
+                </div>
             </div>
         );
     }
